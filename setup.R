16fbfa3bd71ef5b006780ebf248af7cfec08fb15
David Mertz <mertz@kdm.training>
2020-06-01 15:16:21 -0400

    Add setup files

---
+++ setup.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env R
+install.packages("tidyverse")
+install.packages('RPostgreSQL')
+install.packages("BiocManager")
+BiocManager::install("rhdf5")
+install.packages("smotefamily")
+install.packages("imager")
+install.packages("gridExtra")
+install.packages("haven")
